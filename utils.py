1f6f56b29cd5008f2e1558a32171c82cb902a4f0
gev <gevorg2310@yandex.ru>
2025-01-27 15:55:17 +0300

    FIX. Fix readme

--- utils.py
+++ utils.py
@@ -17,21 +17,18 @@ def pagination_params(
 
 pagination_dep = Annotated[Paginator, Depends(pagination_params)]
 
+# openapi schema hack for fastapi Depends()
 class Stub:
     def __init__(self, dependency: Callable[..., Any]) -> None:
-        """Сохраняем нашу абстракцию."""
         self._dependency = dependency
 
     def __call__(self) -> None:
-        """Выкинем ошибку, если забыли подменить реализацию при старте приложения."""
         raise NotImplementedError(f"You forgot to register `{self._dependency}` implementation.")
 
     def __hash__(self) -> int:
-        """Обманываем app.dependency_overrides, чтобы он считал Stub реальной зависимостью"""
         return hash(self._dependency)
 
     def __eq__(self, __value: object) -> bool:
-        """Обманываем app.dependency_overrides, чтобы он считал Stub реальной зависимостью"""
         if isinstance(__value, Stub):
             return self._dependency == __value._dependency
         else:
