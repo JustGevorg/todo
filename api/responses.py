d67bd2e2ef2a61299064d4dea230ad9178135a08
gev <gevorg2310@yandex.ru>
2025-01-27 15:57:32 +0300

    FIX. Fix responses

--- api/responses.py
+++ api/responses.py
@@ -1,4 +1,8 @@
 from resources import ApiResponsesDescriptions, ExceptionsDescriptionsTemplates
+from schemas.tasks import ReadTaskSchema
+
+example_tasks = [ReadTaskSchema(name="Подготовиться к экзамену", description="Изучить конспект", done=False).model_dump(),
+                 ReadTaskSchema(name="Выбросить мусор", description="Выбросить пакет и коробки", done=True).model_dump(),]
 
 create_new_task_responses = {
     200: {"description": ApiResponsesDescriptions.task_created_successfully,
@@ -16,5 +20,47 @@ create_new_task_responses = {
 }
 
 delete_task_responses = {
+    404: {"description": ApiResponsesDescriptions.tasks_not_found,
+          "content": {
+              "application/json": {
+                  "example": {"description": ExceptionsDescriptionsTemplates.updated_task_not_exists.format(
+                      task_name="Сходить в магазин")}
+              }
+          }},
+    200: {"description": ApiResponsesDescriptions.task_deleted_successfully,
+          "content": {
+              "application/json": {
+                  None}
+          }},
+}
 
+update_task_responses = {
+    404: {"description": ApiResponsesDescriptions.tasks_not_found,
+          "content": {
+              "application/json": {
+                  "example": {"description": ExceptionsDescriptionsTemplates.updated_task_not_exists.format(
+                      task_name="Сходить в магазин")}
+              }
+          }},
+    200: {"description": ApiResponsesDescriptions.task_updated_successfully,
+          "content": {
+              "application/json": {
+                  None}
+          }},
+}
+
+
+get_task_responses = {
+    404: {"description": ApiResponsesDescriptions.tasks_not_found,
+          "content": {
+              "application/json": {
+                  "example": {"description": ExceptionsDescriptionsTemplates.tasks_not_found}
+              }
+          }},
+    200: {"description": ApiResponsesDescriptions.requested_tasks,
+          "content": {
+              "application/json": {
+                  "example": example_tasks
+              }
+          }},
 }
